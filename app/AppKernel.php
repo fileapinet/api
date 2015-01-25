52c626de84d747ba5a6a4f9cdafdff39f313eab3
Amy Boyd <amy@amyboyd.co.uk>
2015-01-25 01:26:05 +0000

    Remove unused Swift Mailer.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,7 +12,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new Partnermarketing\FileSystemBundle\PartnermarketingFileSystemBundle(),
             new Liip\DoctrineCacheBundle\LiipDoctrineCacheBundle(),
